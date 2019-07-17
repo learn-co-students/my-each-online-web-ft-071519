549aed2d1e73423a8101c140751370172a642079
Carl A Palumbo <carlpalumbo1@gmail.com>
2019-07-17 21:35:26 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  i = 0
+  new_array = []
+  while i < array.length do 
+    yield array[i]
+    i += 1
+  end 
+  array
 end
+
