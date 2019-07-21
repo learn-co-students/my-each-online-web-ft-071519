0ae1d83d17edc7905aa3dbdfd1430620631b05d1
Emily Lillie <emilylillie3@gmail.com>
2019-07-21 20:09:56 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,10 @@
 
-def my_each(array)
-  i = 0 
-    while i < array.length 
-      yield(array[i])
-      i = i + 1 
-    end
-    array
- end
+def my_each(collection)
+  i = 0
+  while i < collection.length
+    yield(collection[i])
+    i = i + 1
+  end
+  collection
+end
 
